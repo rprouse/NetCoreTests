25651e00364d1fc546ec776b67f3d223a74992d5
Rob Prouse <rob@prouse.org>
2016-10-31 21:17:04 -0400

    Simple .NET Core NUnit Tests

---
+++ test/NUnit.Test.One/EnumToStringConverterTests.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Utilities;
+
+namespace NUnit.Test.One
+{
+    public enum TestTypes
+    {
+        none = 0,
+        a,
+        B,
+        Testing,
+        UnitTesting,
+        integrationTesting
+    }
+
+    [TestFixture]
+    public class EnumToStringConverterTests
+    {
+        [TestCase((TestTypes)10, "")]
+        [TestCase(TestTypes.a, "A")]
+        [TestCase(TestTypes.B, "B")]
+        [TestCase(TestTypes.none, "None")]
+        [TestCase(TestTypes.Testing, "Testing")]
+        [TestCase(TestTypes.UnitTesting, "Unit Testing")]
+        [TestCase(TestTypes.integrationTesting, "Integration Testing")]
+        public void CanConvertEnumIntoFriendlyString(TestTypes value, string expected)
+        {
+            // Arrange/Act
+            var actual = value.ToFriendlyString();
+
+            // Assert
+            Assert.That(actual, Is.Not.Null.And.EqualTo(expected));
+        }
+
+        [TestCase((TestTypes)10, ExpectedResult = "")]
+        [TestCase(TestTypes.a, ExpectedResult = "A")]
+        [TestCase(TestTypes.B, ExpectedResult = "B")]
+        [TestCase(TestTypes.none, ExpectedResult = "None")]
+        [TestCase(TestTypes.Testing, ExpectedResult = "Testing")]
+        [TestCase(TestTypes.UnitTesting, ExpectedResult = "Unit Testing")]
+        [TestCase(TestTypes.integrationTesting, ExpectedResult = "Integration Testing")]
+        public string CanConvertEnumIntoFriendlyString(TestTypes value)
+        {
+            return value.ToFriendlyString();
+        }
+    }
+}
