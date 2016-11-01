b1d467fe0f17b644bd6e5dd9f109692eef284f09
Rob Prouse <rob@prouse.org>
2016-10-31 21:52:06 -0400

    Added an xUnit test project

---
+++ test/XUnit.Test.One/EnumToStringConverterTests.cs
@@ -0,0 +1,36 @@
+﻿using Utilities;
+using Xunit;
+
+namespace XUnit.Test.One
+{
+    public enum TestTypes
+    {
+        none = 0,
+        a,
+        B,
+        Testing,
+        UnitTesting,
+        integrationTesting
+    }
+
+    public class EnumToStringConverterTests
+    {
+        [Theory]
+        [InlineData((TestTypes)10, "")]
+        [InlineData(TestTypes.a, "A")]
+        [InlineData(TestTypes.B, "B")]
+        [InlineData(TestTypes.none, "None")]
+        [InlineData(TestTypes.Testing, "Testing")]
+        [InlineData(TestTypes.UnitTesting, "Unit Testing")]
+        [InlineData(TestTypes.integrationTesting, "Integration Testing")]
+        public void CanConvertEnumIntoFriendlyString(TestTypes value, string expected)
+        {
+            // Arrange/Act
+            var actual = value.ToFriendlyString();
+
+            // Assert
+            Assert.NotNull(actual);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
