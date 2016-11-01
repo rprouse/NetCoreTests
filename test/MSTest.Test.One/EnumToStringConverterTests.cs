7697d5961864baa12c9940fe4a37af0c7be6eda9
Rob Prouse <rob@prouse.org>
2016-10-31 21:38:12 -0400

    Added MSTest project

---
+++ test/MSTest.Test.One/EnumToStringConverterTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Utilities;
+
+namespace MSTest.Test.One
+{
+    public enum TestTypes
+    {
+        none = 0,
+        a,
+        B,
+        Testing,
+        UnitTesting,
+        integrationTesting
+    }
+
+    [TestClass]
+    public class EnumToStringConverterTests
+    {
+        [DataTestMethod]
+        [DataRow((TestTypes)10, "")]
+        [DataRow(TestTypes.a, "A")]
+        [DataRow(TestTypes.B, "B")]
+        [DataRow(TestTypes.none, "None")]
+        [DataRow(TestTypes.Testing, "Testing")]
+        [DataRow(TestTypes.UnitTesting, "Unit Testing")]
+        [DataRow(TestTypes.integrationTesting, "Integration Testing")]
+        public void CanConvertEnumIntoFriendlyString(TestTypes value, string expected)
+        {
+            // Arrange/Act
+            var actual = value.ToFriendlyString();
+
+            // Assert
+            Assert.IsNotNull(actual);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
--- test/NUnit.Test.One/EnumToStringConverterTests.cs
+++ test/NUnit.Test.One/EnumToStringConverterTests.cs
@@ -1,8 +1,4 @@
 ﻿using NUnit.Framework;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Utilities;
 
 namespace NUnit.Test.One
